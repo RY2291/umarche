0fe440a915ae3afb291b0b64c08f203750ba15a5
RY2291 <prgr2498@gmail.com>
2023-04-18 23:28:57 +0900

    変数で設定

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,5 +3,5 @@
     ヘッダー1
   </x-slot>
 コンポーネント1
-  <x-tests.card title="タイトル1" content="本分1"></x-tests.card>
+  <x-tests.card title="タイトル1" content="本分1" :message="$message"></x-tests.card>
 </x-tests.app>

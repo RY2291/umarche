408e8d488de80d72b125f42f44993cf6673d02e7
RY2291 <prgr2498@gmail.com>
2023-04-20 23:24:04 +0900

    classComponent

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,1 +1,10 @@
 コンポーネント2
+<x-tests.app>
+  <x-slot name="header">ヘッダー２</x-slot>
+
+  コンポーネント2
+  <x-test-class-base classBaseMessage="mesee" />
+  <div class="mb-4"></div>
+  <x-test-class-base classBaseMessage="mesee" defaultMessage="初期値から変更している" />
+
+</x-tests.app>

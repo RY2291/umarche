072ecf020c8c0ccf66fe6af41285aaf28df133a4
RY2291 <prgr2498@gmail.com>
2023-04-25 19:41:06 +0900

    serviceContainerTest

---
+++ app/Http/Controllers/LifeCycleContainer.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LifeCycleContainer extends Controller{
+
+    public function showServiceContainerTest(){
+
+        app()->bind('lifeCycle', function(){
+            return 'ライフサイクル';
+        });
+
+        $test = app()->make('lifeCycle');
+
+        // サービスコンテナなし
+        // $message = new Message();
+        // $sample = new Sample($message);
+        // $sample->run();
+        
+        // サービスコンテナあり
+        app()->bind('sample', Sample::class);
+        $sample = app()->make('sample');
+        $sample->run();
+
+        dd($test ,app());
+    }
+}
+
+
+class Sample{
+    
+    public $message;
+
+    public function __construct(Message $message){
+
+        $this->message = $message;
+    }
+
+    public function run(){
+
+        $this->message->send();
+    }
+}
+
+class Message{
+
+    public function send(){
+
+        echo 'メッセージ表示';
+    }
+}
